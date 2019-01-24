d838993c70504fe6d02fbb9f9450ced8828168ac
Pandu Rijal Pasa <pandurijal96@gmail.com>
2019-01-24 18:38:20 +0700

    Updates

---
+++ precache-manifest.f5fba5d248a114910d513265c8590d68.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c46b92b5acec40440701",
+    "url": "/tokoflix/static/js/runtime~main.c46b92b5.js"
+  },
+  {
+    "revision": "d6873a8d99681e073e10",
+    "url": "/tokoflix/static/js/main.d6873a8d.chunk.js"
+  },
+  {
+    "revision": "4356f227b1695d630a39",
+    "url": "/tokoflix/static/js/1.4356f227.chunk.js"
+  },
+  {
+    "revision": "d6873a8d99681e073e10",
+    "url": "/tokoflix/static/css/main.1b0d09ed.chunk.css"
+  },
+  {
+    "revision": "3696f0afb3dbc8cf36f04969c8a0a7a2",
+    "url": "/tokoflix/index.html"
+  }
+];
